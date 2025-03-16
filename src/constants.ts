278e6df692ad9a89f6aafdd1e271114e0eeb38e3
thirdmadman <thirdmadman@gmail.com>
2025-03-16 17:53:58 +0100

    feat: add envs

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = 'Fetch my coin';
+
+export const YH_API_URL = import.meta.env.VITE_YH_API_URL ?? 'https://app.youhodler.com/api/';
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+type ImportMetaEnvVariable = string | undefined;
+
+interface ImportMetaEnv {
+  readonly VITE_YH_API_URL: ImportMetaEnvVariable;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
