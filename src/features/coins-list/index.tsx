d41bbda6fd831263c722acd2e07daff2f75d68c4
thirdmadman <thirdmadman@gmail.com>
2025-03-17 22:07:17 +0100

    feat: add ticker icons

--- src/features/coins-list/index.tsx
+++ src/features/coins-list/index.tsx
@@ -1,3 +1,4 @@
+import { getTickerIcon } from '@/shared/utils/getTickerIcon';
 import { IGetRatesResponse, TCoinRates } from '@/types/IGetRatesResponse';
 import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@heroui/react';
 
@@ -23,12 +24,16 @@ export function CoinsList({ rates }: ICoinsListProps) {
   return (
     <Table aria-label="Example static collection table">
       <TableHeader>
+        <TableColumn>icon</TableColumn>
         <TableColumn>Coin</TableColumn>
         <TableColumn>Rate in USD</TableColumn>
       </TableHeader>
       <TableBody>
         {Object.entries(rates).map(([coin, ratesObject]) => (
-          <TableRow key="1">
+          <TableRow key={coin}>
+            <TableCell>
+              <img width={32} height={32} src={getTickerIcon(coin)} alt={coin} />
+            </TableCell>
             <TableCell>{coin}</TableCell>
             <TableCell>{ratesObject && getRate(ratesObject)}</TableCell>
           </TableRow>
